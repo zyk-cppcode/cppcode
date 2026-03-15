f4f77ba47fb37a0d49accbc13c82bc52111a0a4a
zyk <2118625489@qq.com>
2026-03-15 19:08:29 +0800

    完成 buffer，并测试

--- ReactorServer/Buffer.h
+++ ReactorServer/Buffer.h
@@ -1,12 +1,12 @@
 #pragma once
 
+#include <algorithm>
 #include <assert.h>
 #include <cstdint>
 #include <iostream>
 #include <sys/types.h>
 #include <unistd.h>
 #include <vector>
-
 class Buffer {
 public:
   Buffer() : _buffer(1024), _readOffset(0), _writeOffset(0) {};
@@ -22,40 +22,58 @@ public:
   // 获取可读数据大小
   uint64_t getReadableSize() { return _writeOffset - _readOffset; }
 
+  char &operator[](int index) { return _buffer[index]; }
   // 将读偏移向后移动
   void moveReadOffset(uint64_t size) {
     assert(size <= getReadableSize());
     _readOffset += size;
   }
-  // 将写偏移向后移动(整体空闲空间够了就移动数据，否则就扩容)
+  // 将写偏移向后移动
   void moveWriteOffset(uint64_t size) {
     assert(size <= getFrontFreeSize());
     _writeOffset += size;
   }
+  // 获取总空间
+  uint64_t getTotalSize() { return _buffer.size(); }
+  // 获取可写空间
+  uint64_t getWritableSize() { return getTotalSize() - getReadableSize(); }
   // 确保可写空间足够(整体空闲空间够了就移动数据，否则就扩容)
   void ensureWriteSize(uint64_t size) {
     if (size <= getFrontFreeSize()) {
       return;
-    } 
-    else if (size <= getFrontFreeSize() + getRearFreeSize()) {
+    } else if (size <= getFrontFreeSize() + getRearFreeSize()) {
       std::copy(_buffer.begin() + _readOffset, _buffer.begin() + _writeOffset,
                 _buffer.begin());
-      _writeOffset -= _readOffset;
+      _writeOffset = getReadableSize();
       _readOffset = 0;
-      _writeOffset += size;
-    }
-     else {
+
+    } else {
       _buffer.resize(_buffer.size() + size -
-                      (getFrontFreeSize() + getRearFreeSize()));
-      _writeOffset += size;
+                     (getFrontFreeSize() + getRearFreeSize()));
     }
   }
   // 写入数据
-  void write(const char *data, uint64_t len) {}
+  void write(const char *data, uint64_t len) {
+    ensureWriteSize(len);
+    std::copy(data, data + len, getWriteOffset());
+    moveWriteOffset(len);
+  }
+  void write(std::string str) { write(&str[0], str.size()); }
   // 读取数据
-  void read(char *buf, uint64_t len) {}
+  void read(char *buf, uint64_t len) {
+    if (len > getReadableSize()) {
+      std::cout << "可读数据不够" << std::endl;
+      return;
+    }
+    std::copy(getReadOffset(), getReadOffset() + len, buf);
+    moveReadOffset(len);
+  }
   // 清空缓冲区
-  void clear() {}
+  void clear() {
+    _writeOffset = 0;
+    _readOffset = 0;
+  }
+
 private:
   std::vector<char> _buffer;
   uint64_t _writeOffset;
---
+++ ReactorServer/logger.hpp
@@ -0,0 +1,230 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <filesystem> // C++17 文件操作
+#include <fstream>
+#include <ctime>
+#include <unistd.h>
+#include <memory>
+#include <sstream>
+#include "Mutex.hpp"
+
+// 规定日志等级
+enum class LogLevel
+{
+    DEBUG,
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
+};
+
+std::string Level2String(LogLevel level)
+{
+    switch (level)
+    {
+    case LogLevel::DEBUG:
+        return "Debug";
+    case LogLevel::INFO:
+        return "Info";
+    case LogLevel::WARNING:
+        return "Warning";
+    case LogLevel::ERROR:
+        return "Error";
+    case LogLevel::FATAL:
+        return "Fatal";
+    default:
+        return "Unknown";
+    }
+}
+
+// 20XX-08-04 12:27:03
+std::string GetCurrentTime()
+{
+    // 1. 获取时间戳
+    time_t currtime = time(nullptr);
+
+    // 2. 如何把时间戳转换成为20XX-08-04 12:27:03
+    struct tm currtm;
+    localtime_r(&currtime, &currtm);
+
+    // 3. 转换成为字符串 -- dubug?
+    char timebuffer[64];
+    snprintf(timebuffer, sizeof(timebuffer), "%4d-%02d-%02d %02d:%02d:%02d",
+             currtm.tm_year + 1900,
+             currtm.tm_mon + 1,
+             currtm.tm_mday,
+             currtm.tm_hour,
+             currtm.tm_min,
+             currtm.tm_sec);
+
+    return timebuffer;
+}
+
+///////////////////////////////////////////////////////////////////
+// 1. 刷新的问题 -- 假设我们已经有了一条完整的日志，string->设备(显示器，文件)
+// 基类方法
+class LogStrategy
+{
+public:
+    virtual ~LogStrategy() = default;
+    virtual void SyncLog(const std::string &logmessage) = 0;
+};
+
+// 显示器刷新
+class ConsoleLogStrategy : public LogStrategy
+{
+public:
+    ~ConsoleLogStrategy()
+    {
+    }
+    void SyncLog(const std::string &logmessage) override
+    {
+        {
+            LockGuard lockguard(&_lock);
+            std::cout << logmessage << std::endl;
+        }
+    }
+
+private:
+    Mutex _lock;
+};
+
+const std::string logdefaultdir = "log";
+const static std::string logfilename = "test.log";
+
+// 文件刷新
+class FileLogStrategy : public LogStrategy
+{
+public:
+    FileLogStrategy(const std::string &dir = logdefaultdir,
+                    const std::string filename = logfilename)
+        : _dir_path_name(dir), _filename(filename)
+    {
+        LockGuard lockguard(&_lock);
+        if (std::filesystem::exists(_dir_path_name))
+        {
+            return;
+        }
+        try
+        {
+            std::filesystem::create_directories(_dir_path_name);
+        }
+        catch (const std::filesystem::filesystem_error &e)
+        {
+            std::cerr << e.what() << "\r\n";
+        }
+    }
+    void SyncLog(const std::string &logmessage) override
+    {
+        {
+            LockGuard lockguard(&_lock);
+            std::string target = _dir_path_name;
+            target += "/";
+            target += _filename;
+
+            std::ofstream out(target.c_str(), std::ios::app); // append
+            if (!out.is_open())
+            {
+                return;
+            }
+            out << logmessage << "\n"; // out.write
+            out.close();
+        }
+    }
+
+    ~FileLogStrategy()
+    {
+    }
+
+private:
+    std::string _dir_path_name; // log
+    std::string _filename;      // hello.log => log/hello.log
+    Mutex _lock;
+};
+
+// 网络刷新
+
+////////////////////////////////////////////////////////
+
+// 1. 定制刷新策略
+// 2. 构建完整的日志
+class Logger
+{
+public:
+    Logger()
+    {
+    }
+    void EnableConsoleLogStrategy()
+    {
+        _strategy = std::make_unique<ConsoleLogStrategy>();
+    }
+    void EnableFileLogStrategy()
+    {
+        _strategy = std::make_unique<FileLogStrategy>();
+    }
+    // 形成一条完整日志的方式
+    class LogMessage
+    {
+    public:
+        LogMessage(LogLevel level, std::string &filename, int line, Logger &logger)
+        : _curr_time(GetCurrentTime()),
+          _level(level),
+          _pid(getpid()),
+          _filename(filename),
+          _line(line),
+          _logger(logger)
+        {
+            std::stringstream ss;
+            ss << "[" << _curr_time << "] "
+               << "[" << Level2String(_level) << "] "
+               << "[" << _pid << "] "
+               << "[" << _filename << "] "
+               << "[" << _line << "]"
+               <<  " >>> ";
+            _loginfo = ss.str();
+        }
+        template<typename T>
+        LogMessage& operator << (const T &info)
+        {
+            std::stringstream ss;
+            ss << info;
+            _loginfo += ss.str();
+            return *this;
+        }
+
+        ~LogMessage()
+        {
+            if(_logger._strategy)
+            {
+                _logger._strategy->SyncLog(_loginfo);
+            }
+        }
+    private:
+        std::string _curr_time; // 日志时间
+        LogLevel _level; // 日志等级
+        pid_t _pid; // 进程pid
+        std::string _filename;
+        int _line;
+
+        std::string _loginfo; // 一条合并完成的，完整的日志信息
+        Logger &_logger; // 提供刷新策略的具体做法
+    };
+    LogMessage operator()(LogLevel level, std::string filename, int line)
+    {
+        return LogMessage(level, filename, line, *this);
+    } 
+    ~Logger()
+    {
+    }
+
+private:
+    std::unique_ptr<LogStrategy> _strategy;
+};
+
+Logger logger;
+
+#define LOG(level) logger(level, __FILE__, __LINE__)
+#define EnableConsoleLogStrategy() logger.EnableConsoleLogStrategy()
+#define EnableFileLogStrategy() logger.EnableFileLogStrategy()
